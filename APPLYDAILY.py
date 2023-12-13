751bcc337d41d31c2bd316653bf8e3d9053f2817
RSDCFGVHBJNKML <137018415+RSDCFGVHBJNKML@users.noreply.github.com>
2023-12-13 13:59:46 +0800

    Update APPLYDAILY.py

--- APPLYDAILY.py
+++ APPLYDAILY.py
@@ -1,10 +1,19 @@
-import os
 import requests
+import os
 
 url = "https://www.north-plus.net/plugin.php"
+params = {
+    "H_name": "tasks",
+    "action": "ajax",
+    "actions": "job",
+    "cid": "14",
+    "nowtime": "1702389727027",
+    "verify": "ca81e905"
+}
+cookie = os.environ.get("COOKIE")
+
 headers = {
-    "Host": "www.north-plus.net",
-    "Cookie": os.environ["COOKIE"],
+    "Cookie": cookie,
     "Sec-Ch-Ua": '"Chromium";v="109", "Not_A Brand";v="99"',
     "Sec-Ch-Ua-Mobile": "?0",
     "Sec-Ch-Ua-Platform": '"Windows"',
@@ -13,21 +22,13 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Sec-Fetch-Site": "same-origin",
     "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
     "Sec-Fetch-Dest": "iframe",
-    "Referer": "https://www.north-plus.net/plugin.php?H_name-tasks.html.html",
+    "Referer": "https://www.north-plus.net/plugin.php?H_name=tasks.html",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "zh-CN,zh;q=0.9"
 }
 
-params = {
-    "H_name": "tasks",
-    "action": "ajax",
-    "actions": "job",
-    "cid": "15",
-    "nowtime": "1702446268898",
-    "verify": "ca81e905"
-}
-
 response = requests.get(url, headers=headers, params=params)
 
-print(response.text)  # 打印响应内容
+print(response.text)
