de82759a515c66478e2157fec0ff46b433f0b979
Joshua <137018415+Joshua-auhsoj@users.noreply.github.com>
2023-12-18 13:42:02 +0800

    Add files via upload

--- APPLYDAILY.py
+++ APPLYDAILY.py
@@ -3,17 +3,23 @@ import os
 import xml.etree.ElementTree as ET
 
 cookie_value = os.getenv('COOKIE')
-
 cookies = {cookie.split('=')[0]: cookie.split('=')[1] for cookie in cookie_value.split('; ')}
 
 headers = {
-    'DNT': '1',
-    'Referer': 'https://www.south-plus.net/plugin.php?H_name-tasks.html',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'authority': 'www.south-plus.net',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7',
+    'dnt': '1',
+    'referer': 'https://www.south-plus.net/plugin.php?H_name-tasks.html.html',
     'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'iframe',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
 }
 
 params = {
@@ -21,7 +27,7 @@ params = {
     'action': 'ajax',
     'actions': 'job',
     'cid': '15',
-    'nowtime': '1702806835359',
+    'nowtime': '1702877397639',
     'verify': '9d5c5785',
 }
 
