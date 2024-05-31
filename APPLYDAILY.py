e1e16eb82fd741b7aefbd48a85dfd2f48ad7023f
Joshua <137018415+Joshua-auhsoj@users.noreply.github.com>
2024-05-31 22:52:32 +0800

    Update APPLYDAILY.py

--- APPLYDAILY.py
+++ APPLYDAILY.py
@@ -8,29 +8,35 @@ cookies = {cookie.split('=')[0]: cookie.split('=')[1] for cookie in cookie_value
 
 
 headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7',
     'dnt': '1',
     'priority': 'u=0, i',
     'referer': 'https://www.south-plus.net/plugin.php?H_name-tasks.html',
-    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-arch': '"x86"',
+    'sec-ch-ua-bitness': '"64"',
+    'sec-ch-ua-full-version': '"125.0.6422.142"',
+    'sec-ch-ua-full-version-list': '"Google Chrome";v="125.0.6422.142", "Chromium";v="125.0.6422.142", "Not.A/Brand";v="24.0.0.0"',
     'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
     'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform-version': '"15.0.0"',
     'sec-fetch-dest': 'iframe',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
 }
+
 
 params = {
     'H_name': 'tasks',
     'action': 'ajax',
     'actions': 'job',
     'cid': '15',
-    'nowtime': '1716115922180',
-    'verify': '6dda1f22',
+    'nowtime': '1717166831730',
+    'verify': '5af36471',
 }
 
 response = requests.get('https://www.south-plus.net/plugin.php', params=params, cookies=cookies, headers=headers)
